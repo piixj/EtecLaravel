b0183877d0e8519b6222aa69f5c3adc34f383161
piixj <kauansouza2026@gmail.com>
2025-03-20 20:42:40 -0300

    Laravel

--- projeto2/routes/web.php
+++ projeto2/routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/', [UserController::class, 'index'])->name('user.index');function () {
     return view('welcome');
-});
+};
